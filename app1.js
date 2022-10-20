358f12450d43e8a1200a1df0759f7500b9bfbade
Danielle <dsg1457.dg@gmail.com>
2022-10-20 14:08:10 -0600

    files

--- app1.js
+++ app1.js
@@ -4,3 +4,35 @@ const myDog = {
     tails: 1,
     friends: ["Toys", "Dogs"]
   };
+
+  const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  // Only change code below this line
+  const hatValue = testObj.hat;      // Change this line
+  const shirtValue = testObj.shirt;
+
+  const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Only change code below this line
+  const entreeValue = testObj["an entree"];//change this line
+  const drinkValue = testObj['the drink'];    // Change this line
+
+  // Setup
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  // Only change code below this line
+  const playerNumber = 16;  // Change this line
+  const player = testObj[playerNumber];
+    // Change this 
